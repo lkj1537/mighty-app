a13fef172cd3dcc8671f8768161b07b9c94babcd
lkj1537 <56630107+lkj1537@users.noreply.github.com>
2026-02-19 11:08:20 +0900

    first

---
+++ config.js
@@ -0,0 +1,4 @@
+// 설정 파일: 앞으로 구글 URL이 바뀌면 여기만 수정하세요!
+const CONFIG = {
+    GAS_URL: "https://script.google.com/macros/s/AKfycbz3r7wGSDYiF-toKqP3DhmayN2z6__ky92oNAvZsgQWn_msi7ZBl6WCD-OMiUo_Prg/exec"
+};
